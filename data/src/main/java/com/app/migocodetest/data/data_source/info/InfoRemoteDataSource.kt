4f6b9c9ae49e5d68857517b9d3a358f63fdf3a91
戴君維 <billdai0624@gmail.com>
2021-06-21 03:40:01 +0800

    fix(InfoRemoteDataSource): Add missing scheduler control

--- data/src/main/java/com/app/migocodetest/data/data_source/info/InfoRemoteDataSource.kt
+++ data/src/main/java/com/app/migocodetest/data/data_source/info/InfoRemoteDataSource.kt
@@ -2,15 +2,16 @@ package com.app.migocodetest.data.data_source.info
 
 import com.app.migocodetest.data.dto.ApiResult
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 class InfoRemoteDataSource @Inject constructor(private val infoApiService: InfoApiService) :
     InfoDataSource {
     override fun getPublicStatusInfo(): Single<ApiResult> {
-        return infoApiService.getPublicStatusInfo()
+        return infoApiService.getPublicStatusInfo().subscribeOn(Schedulers.io())
     }
 
     override fun getPrivateStatusInfo(): Single<ApiResult> {
-        return infoApiService.getPrivateStatusInfo()
+        return infoApiService.getPrivateStatusInfo().subscribeOn(Schedulers.io())
     }
 }
