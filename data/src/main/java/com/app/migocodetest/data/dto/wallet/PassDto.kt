489ad5af5b9cafcf1cedbe9dee3076ff6c4bd0d3
戴君維 <billdai0624@gmail.com>
2021-06-24 04:34:26 +0800

    refactor: Optimize import

--- data/src/main/java/com/app/migocodetest/data/dto/wallet/PassDto.kt
+++ data/src/main/java/com/app/migocodetest/data/dto/wallet/PassDto.kt
@@ -3,7 +3,6 @@ package com.app.migocodetest.data.dto.wallet
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.app.migocodetest.domain.entity.wallet.PassEntity
 
 @Entity(tableName = "passes")
 data class PassDto(
--- domain/src/main/java/com/app/migocodetest/domain/use_case/wallet/GetPassListObservableUseCase.kt
+++ domain/src/main/java/com/app/migocodetest/domain/use_case/wallet/GetPassListObservableUseCase.kt
@@ -3,7 +3,6 @@ package com.app.migocodetest.domain.use_case.wallet
 import com.app.migocodetest.domain.entity.wallet.PassEntity
 import com.app.migocodetest.domain.repository.wallet.IWalletRepository
 import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.core.Single
 
 class GetPassListObservableUseCase(private val walletRepository: IWalletRepository) {
     operator fun invoke(): Flowable<List<PassEntity>> {
